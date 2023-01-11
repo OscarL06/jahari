554022b2dbfedb8fdebe677e8d672ef9d21148b0
Oscar <oscarl06@gmail.com>
2023-01-10 21:10:27 -0600

    Added link to main page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,6 +35,12 @@
 
         <div class="flex flex-col items-start w-full">
             <div class="flex flex-col items-start w-full px-2 mt-10 space-y-1 text-lg">
+                <a href="/" class="flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                    JahariStampley.com
+                </a>
                 <a href="" class="flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -89,6 +95,12 @@
         </div>
 
         <div class="flex flex-col items-start w-full px-2 mt-10 space-y-1 text-lg">
+            <a href="/" class="flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+                JahariStampley.com
+            </a>
             <a href="" class="flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
