47c7e50ad1e62a2fe826311452d3fe80894da9f5
Oscar <oscarl06@gmail.com>
2023-01-22 17:58:44 -0600

    Layout updated

--- resources/views/courses/lessons.blade.php
+++ resources/views/courses/lessons.blade.php
@@ -6,35 +6,74 @@
     <div class="w-full lg:w-4/5 flex flex-col items-center lg:ml-[20%] mt-16 lg:mt-6">
         <h1 class="text-5xl tracking-tighter font-abel">{{ $lesson->course->name }} | {{ $lesson->name }}</h1>
 
-        
-        @livewire('progress-video', ['lesson_id' => $lesson->id ])
+        <div class="flex flex-col items-center w-11/12 md:flex-row md:justify-center">
+            <div class="flex flex-col items-center w-11/12 mt-16 xl:w-2/3 md:w-1/2">
+                @livewire('progress-video', ['lesson_id' => $lesson->id ])
 
-        <div class="flex justify-center w-5/6 p-1 mt-2 space-x-10 text-white rounded-md shadow lg:w-3/5 bg-main" wire:click="updateProgress">
-            <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                </svg>
-                Complete
-            </button>
-            <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                </svg>
-                Notes
-            </button>
-            <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
-                </svg>
-                Practice
-            </button>
-            <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
-                </svg>
-                Reset
-            </button>
+                <div class="flex justify-center w-full p-1 mt-2 space-x-5 text-white rounded-md shadow md:space-x-2 xl:space-x-10 lg:w-full bg-main" wire:click="updateProgress">
+                    <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg>
+                        Complete
+                    </button>
+                    <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                        Notes
+                    </button>
+                    <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
+                        </svg>
+                        Practice
+                    </button>
+                    <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                        </svg>
+                        Reset
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center justify-start w-11/12 h-full mt-8 lg:mt-28 md:w-1/2 xl:w-1/3 font-abel" x-data="{ open: false }">
+                <div class="flex justify-center w-full">
+                    <button class="w-1/4 py-1 mr-3 text-white rounded-md shadow bg-main" @click="open = false">Lessons</button>
+                    <button class="w-1/4 py-1 text-white rounded-md shadow bg-main" @click="open =  true">Notes</button>
+                </div>
+
+                <div class="flex justify-center w-full mt-3 rounded-md h-4/5" x-show="!open">
+                    <div class="flex flex-col w-5/6 p-2 space-y-4 rounded-md shadow font-abel">
+                        <a href="" class="w-full px-1 py-1 text-lg rounded-md odd:bg-purple-50 hover:bg-purple-100">
+                            <span class="px-2 py-1 mr-1 text-sm text-white rounded-full bg-main">1</span>
+                            Name of Lesson
+                        </a>
+                        <a href="" class="w-full px-1 py-1 text-lg rounded-md odd:bg-purple-50 hover:bg-purple-100">
+                            <span class="px-2 py-1 mr-1 text-sm text-white rounded-full bg-main">1</span>
+                            Name of Lesson
+                        </a>
+                        <a href="" class="w-full px-1 py-1 text-lg rounded-md odd:bg-purple-50 hover:bg-purple-100">
+                            <span class="px-2 py-1 mr-1 text-sm text-white rounded-full bg-main">1</span>
+                            Name of Lesson
+                        </a>
+                        <a href="" class="w-full px-1 py-1 text-lg rounded-md odd:bg-purple-50 hover:bg-purple-100">
+                            <span class="px-2 py-1 mr-1 text-sm text-white rounded-full bg-main">1</span>
+                            Name of Lesson
+                        </a>
+                    </div>
+                </div>
+
+                <div class="flex flex-col items-center w-full mt-3" x-show="open">
+                    <textarea name="" id="" cols="30" rows="10" class="w-5/6 border-none rounded-md shadow" placeholder="Notes"></textarea>
+                    <button class="w-1/4 py-1 mt-2 mr-3 text-white rounded-md shadow bg-main">
+                        Save
+                    </button>
+                </div>
+            </div>
         </div>
+        
 
         <div class="mt-12 text-purple-800">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
--- resources/views/livewire/progress-video.blade.php
+++ resources/views/livewire/progress-video.blade.php
@@ -1,3 +1,3 @@
-<div class="flex justify-center w-5/6 p-5 mt-16 rounded-md shadow lg:w-3/5 bg-main" wire:click="updateProgress">
+<div class="flex justify-center p-5 rounded-md shadow bg-main" wire:click="updateProgress">
     <video id="video" src="/videos/{{ $lesson->video->url }}" class="rounded-md" controls></video>
 </div>
