bbb6ea46b3ade2fd9a16f6cd8e920b2c57b6f110
Oscar <oscarl06@gmail.com>
2023-01-22 20:34:12 -0600

    Create lesson complete component

---
+++ app/Http/Livewire/LessonComplete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LessonComplete extends Component
+{
+    public function render()
+    {
+        return view('livewire.lesson-complete');
+    }
+}
