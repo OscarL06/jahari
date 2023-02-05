f82ff2f2e2c8e8edf76246fd27b29e28ef41c8c4
Oscar <oscarl06@gmail.com>
2023-02-05 15:21:57 -0600

    Notes page added

---
+++ resources/views/notes.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Notes | ' . Auth::user()->username)
+
+@section('content')
+    <div>
+
+    </div>
+
+    <x-back-button class="top-4 right-4"/>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{ProfileController, CategoriesController, SubscribeController};
+use App\Http\Controllers\{ProfileController, CategoriesController, SubscribeController, NotesController};
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,20 +17,21 @@ Route::get('/gallery', function () { return view('jahari.gallery'); })->name('ga
 Route::get('/dash', function () { return view('dash');})->name('dash');
 Route::get('/courses', [CategoriesController::class, 'categories'])->name('categories');
 Route::get('/subscribe', [SubscribeController::class, 'subscribe'])->name('subscribe');
+Route::get('/notes', [NotesController::class, 'show'])->name('notes');
 
 Route::middleware('subscribed')->group(function () {
     Route::get('/courses/{id}', [CategoriesController::class, 'courses'])->name('course');
     Route::get('courses/lesson/{id}', [CategoriesController::class, 'lessons'])->name('lesson');
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); */
 
-Route::middleware('auth')->group(function () {
+/* Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+}); */
 
 require __DIR__.'/auth.php';
