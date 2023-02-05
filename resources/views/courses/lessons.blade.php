32139194d78ce11c4fcd16d3f7904e1fea09279f
Oscar <oscarl06@gmail.com>
2023-02-05 15:01:02 -0600

    Updated notes button

--- resources/views/courses/lessons.blade.php
+++ resources/views/courses/lessons.blade.php
@@ -3,7 +3,7 @@
 @section('title', $lesson->name . ' | ' . Auth::user()->username)
 
 @section('content')
-    <div class="w-full lg:w-4/5 flex flex-col items-center lg:ml-[20%] mt-16 lg:mt-6">
+    <div class="w-full lg:w-4/5 flex flex-col items-center lg:ml-[20%] mt-16 lg:mt-6" x-data="{ open: false }">
 
         <div class="mb-4 w-[95%] bg-gray-200 rounded-full h-2.5 overflow-hidden">
             <div class="bg-main h-2.5 rounded-full animate__animated animate__lightSpeedInLeft animate__slower" style="width: {{ $completed }}%"></div>
@@ -28,12 +28,12 @@
 
                 <div class="flex justify-center w-full p-1 mt-2 space-x-5 text-white rounded-md shadow md:space-x-2 xl:space-x-10 lg:w-full bg-main" wire:click="updateProgress">
                     @livewire('lesson-complete', ['lesson_id' => $lesson->id])
-                    <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
+                    <a href="#notes" class="cursor-pointer px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center" @click="open =  true">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                         </svg>
                         Notes
-                    </button>
+                    </a>
                     <button class="px-2 py-0.5 text-white bg-main rounded-md font-abel shadow shadow-purple-500 flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
@@ -44,10 +44,10 @@
                 </div>
             </div>
 
-            <div class="flex flex-col items-center justify-start w-11/12 h-full mt-8 lg:mt-28 md:w-1/2 xl:w-1/3 font-abel" x-data="{ open: false }">
+            <div class="flex flex-col items-center justify-start w-11/12 h-full mt-8 lg:mt-28 md:w-1/2 xl:w-1/3 font-abel">
                 <div class="flex justify-center w-full">
                     <button class="w-1/4 py-1 mr-3 text-white rounded-md shadow bg-main" @click="open = false">Lessons</button>
-                    <button class="w-1/4 py-1 text-white rounded-md shadow bg-main" @click="open =  true">Notes</button>
+                    <button id="notes" class="w-1/4 py-1 text-white rounded-md shadow bg-main" @click="open =  true">Notes</button>
                 </div>
 
                 <div class="flex justify-center w-full mt-3 rounded-md h-4/5" x-show="!open">
