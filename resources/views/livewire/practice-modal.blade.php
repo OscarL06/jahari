11c707eeaa0f4b90b8de32656d9d2b0bef27f320
Oscar <oscarl06@gmail.com>
2023-02-12 19:57:47 -0600

    Updated

--- resources/views/livewire/practice-modal.blade.php
+++ resources/views/livewire/practice-modal.blade.php
@@ -1,5 +1,5 @@
-<div x-show="practice" x-transition:enter.duration.800ms x-transition:leave.duration.800ms class="fixed inset-0 flex items-center justify-center px-4 bg-black bg-opacity-75 md:px-0">
-    <div class="flex flex-col max-w-lg p-6 bg-white rounded-lg shadow-2xl lg:w-2/3" @click.away="practice = false">
+<div x-show="practice" x-transition:enter.duration.800ms x-transition:leave.duration.800ms class="fixed inset-0 z-30 flex items-center justify-center px-4 bg-black bg-opacity-75 md:px-0">
+    <div class="flex flex-col w-10/12 p-6 bg-white rounded-lg shadow-2xl lg:w-1/3" @click.away="practice = false">
         <div class="flex justify-between mb-4">
             <h3 class="text-3xl font-abel">Practice <span class="text-sm">{{ date("m/d/Y") }}</span></h3>
             <button @click="practice = false">
@@ -11,12 +11,13 @@
         <div class="py-2 rounded-md shadow lg:p-4 watch font-abel bg-main">
             <div wire:model.defer="duration" class="w-full px-2 text-center text-white rounded-lg text-7xl time lg:text-8xl bg-main">{{ $duration }}</div>
             <div class="flex justify-around mt-6 text-black">
-                <button id="start" class="px-2 py-1 bg-white rounded-md lg:px-8">Start</button>
-			    <button id="stop" class="px-2 py-1 bg-white rounded-md lg:px-8">Stop</button>
-			    <button id="reset" class="px-2 py-1 bg-white rounded-md lg:px-8">Reset</button>
+                <button id="start" class="px-4 py-1 bg-white rounded-md lg:px-8">Start</button>
+			    <button id="stop" class="px-4 py-1 bg-white rounded-md lg:px-8">Stop</button>
+			    <button id="reset" class="px-4 py-1 bg-white rounded-md lg:px-8">Reset</button>
             </div>
         </div>
-        <input wire:model="name" type="text" placeholder="Practice Session Name" class="mt-4 border border-purple-700 rounded-md font-abel focus:ring-1 focus:ring-purple-700">
+        <p class="mt-4 font-abel">optional</p>
+        <input wire:model="name" type="text" placeholder="Practice Session Name" class="border border-purple-700 rounded-md font-abel focus:ring-1 focus:ring-purple-700">
         <button id="save" class="w-full py-2 mt-8 text-white rounded-md shadow bg-main hover:opacity-90" wire:click="savePracticeSession">Save</button>
     </div>
 </div>
